7ca8c2ee3c9c87a709944884ba938909488d7b94
Manuel Quiñones <manuq@laptop.org>
2013-05-08 22:45:24 -0300

    Debug: send js console messages to eclipse Log

--- src/org/sugarlabs/clockjs/ClockActivity.java
+++ src/org/sugarlabs/clockjs/ClockActivity.java
@@ -2,10 +2,13 @@ package org.sugarlabs.clockjs;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Window;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+import android.webkit.ConsoleMessage;
 
 public class ClockActivity extends Activity {
 
@@ -28,6 +31,15 @@ public class ClockActivity extends Activity {
         // Make sure links in the webview is handled by the webview
         // and not sent to a full browser
         myWebview.setWebViewClient(new WebViewClient());
+
+        myWebview.setWebChromeClient(new WebChromeClient() {
+          public boolean onConsoleMessage(ConsoleMessage cm) {
+            Log.d("MyApplication", cm.message() + " -- From line "
+                                 + cm.lineNumber() + " of "
+                                 + cm.sourceId() );
+            return true;
+          }
+        });
 
         //And let the fun begin
         myWebview.loadUrl("file:///android_asset/index.html");
