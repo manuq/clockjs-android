16b0e13c93f0a4c52e78026351b0fa78b76d9f20
Manuel Quiñones <manuq@laptop.org>
2013-05-08 21:13:48 -0300

    Initial commit
    For now it just loads the online activity as published in github.io

---
+++ src/org/sugarlabs/clockjs/ClockActivity.java
@@ -0,0 +1,36 @@
+package org.sugarlabs.clockjs;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+
+public class ClockActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Remove title bar as we already have the sugar toolbar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_clock);
+
+        // Configure the webview setup in the xml layout
+        WebView myWebview = (WebView) findViewById(R.id.webview);
+
+        // Allow javascript
+        WebSettings webSettings = myWebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Make sure links in the webview is handled by the webview
+        // and not sent to a full browser
+        myWebview.setWebViewClient(new WebViewClient());
+
+        //And let the fun begin
+        myWebview.loadUrl("http://manuq.github.io/clockjs/");
+    }    
+
+}
